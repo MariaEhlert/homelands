c708d7e9210183cf5b5f3915c2875352a54d6fa8
MariaEhlert <83949695+MariaEhlert@users.noreply.github.com>
2022-09-06 14:36:13 +0200

    burger anmeldelser (forside) færdig

--- src/Components/Comments/Comments.jsx
+++ src/Components/Comments/Comments.jsx
@@ -1,6 +1,7 @@
 import './Comments.scss';
 import React, { useState, useEffect } from "react"
 import axios from "axios";
+import Carousel from 'react-material-ui-carousel';
 export const Comments = () => {
     const [comments, setComments] = useState([]);
     useEffect(() => {
@@ -17,21 +18,27 @@ export const Comments = () => {
         getComments();
     }, [])
     const firstComment = comments.slice(9, 13);
-
     return (
+        <>
         <section className='commentsWrapper'>
-            {firstComment && firstComment.map((item) => {
-                return (
-                    <article key={item.id}>
-                        <h5>{item.title}</h5>
-                        <p>{item.content}</p>
-                        <p>{item.user.firstname}</p>
-                        <p>{item.created_friendly}</p>
-                    </article>
-                )
-            })
+            <Carousel animation="fade" interval="5000" showThumbs={false}>
+                {firstComment && firstComment.map((item) => {
+                    return (
+                        <article key={item.id}>
+                            <h5>{item.title}</h5>
+                            <p>{item.content}</p>
+                            <p>{item.user.firstname}, {item.created_friendly}</p>
+                        </article>
+                    )
+                })
             }
+            </Carousel>
+            
         </section>
+        {/* <section className='submitComment'>
+            <button>Skriv en anmeldelse</button>
+        </section> */}
+        </>
 
     )
 }
