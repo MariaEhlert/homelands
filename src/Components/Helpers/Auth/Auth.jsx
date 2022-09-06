1f25e8ec94448e462e1bd1860a0bd946349939d4
MariaEhlert <83949695+MariaEhlert@users.noreply.github.com>
2022-09-06 14:54:42 +0200

    login funktion (ikke stylet)

---
+++ src/Components/Helpers/Auth/Auth.jsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useEffect, useState } from "react";
+//useContext er en måde at styre tilstande globalt (fx sideskrift)
+//useContext til at transpoter igennem komponeter 
+//så man fx kan fortælle bruger "du er logget ind som {loginData.username}"
+const AuthContext = createContext();
+
+//({children}) er et shotcut for props.children
+//da det kun er props children der skal bruges og
+//der er ingen properties fx props.title 
+const AuthProvider = ({children}) => {
+    const [loginData, setLoginData] = useState('');
+    useEffect(() => {
+        if(sessionStorage.getItem('token')){
+            setLoginData(JSON.parse(sessionStorage.getItem('token')))
+        };
+        //dependency arrays
+    }, [children]);
+    return(
+        //Provider er en metode til at give alle children adgang til loginData (useState hook)
+        //alt der ligger i AuthContext giver adgang til loginData, setLoginData
+        //alt der ligger i appen har adgang til useState hook (loginData)
+        //vi bruger den til at 
+        <AuthContext.Provider value={{loginData, setLoginData}}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
+//custom hooks
+//gør det nemmer at hente rundt omkring
+const useAuth = () => useContext(AuthContext);
+//hvis man skal kalde context 
+//eksempel: const {loginData} = useAuth()
+
+export { AuthContext, AuthProvider, useAuth}
--- src/Components/Sites/Login/Login.jsx
+++ src/Components/Sites/Login/Login.jsx
@@ -1,10 +1,67 @@
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { useAuth } from "../../Helpers/Auth/Auth";
 import { Layout } from "../../Helpers/Layout/Layout"
 
 export const Login = () => {
+        //register og handleSubmit kommer fra useForm
+        const {register, handleSubmit, formState: {errors}} = useForm();
+        // kalder custum state hooks useAuth
+        const {loginData, setLoginData} = useAuth();
+        const sendLoginRequest = async data => { 
+            const formData = new FormData();
+            formData.append('username', data.username);
+            formData.append('password', data.password);
+            const result = await axios.post('https://api.mediehuset.net/token', formData);
+            handleSessionData(result.data);
+        }
+        const handleSessionData = data => {
+            if(data){
+                sessionStorage.setItem('token', JSON.stringify(data))
+                setLoginData(data);
+            }
+    
+        }
+        const logOut = () => {
+            sessionStorage.removeItem('token');
+            setLoginData('');
+        }
     return(
-        <Layout title='Forside' description='forside'>
-            <h1>Login</h1>
-            <p>her kommer mine props children</p>
+        <Layout title='Login' description='login'>
+            {!loginData && !loginData.username ? (
+                <>
+                <h1>Login</h1>
+                {/* closer  */}
+                {/* en måde man sender en function videre */}
+                {/* sendLoginRequest lukker handleSubmit's funktion */}
+                <form onSubmit={handleSubmit(sendLoginRequest)}>
+                        <div>
+                            <input type="text" placeholder="Indtast brugernavn" id="username" {...register("username", {required: true})} />
+                            {/* && hvis er der fejl så skal den sende en fejl besked */}
+                            {errors.username && (
+                                <span>Du skal indtaste dit brugernavn</span>
+                            )}
+                        </div>
+                        <div>
+                            <input type="password" placeholder="Indtast adgangskode" id="password" {...register("password", {required: true})} />
+                            {/* && hvis er der fejl så skal den sende en fejl besked */}
+                            {errors.password && (
+                                <span>Du skal indtaste din adgangskode</span>
+                            )}
+                        </div>
+                        <div>
+                            <button>Login</button>
+                        </div>
+                </form>
+                </>
+            ) : 
+            (
+                <div>
+                    <p>Logget ind som {loginData.username}</p>
+                    <button onClick={logOut}>Log ud</button>
+                </div>
+            )
+            }
         </Layout>
     )
 }
